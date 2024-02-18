9d7cd9fba32077e4733bb4fa58faef00a00a5b37
Jason Shin <tlswoals2006@gmail.com>
2024-02-17 22:37:43 -0800

    learned data fetching in Nextjs 14, like pararell fetching, using suspense in react

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,27 @@
-import Navigation from '../../components/navigation';
+import Link from 'next/link';
 
-export const metadata = {
-  title: 'Home',
-};
+export const metadata = { title: 'Home' };
 
-export default function Tomato() {
+export const MOVIES_URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(MOVIES_URL);
+  const json = await response.json();
+  return json;
+}
+
+// Server component: Caching fetch url.
+
+export default async function HomePage() {
+  const movies = await getMovies();
   return (
     <div>
-      <h1>Hello!</h1>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
     </div>
   );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,42 @@
-export default function MovieDetail({
+import { Suspense } from 'react';
+import { MOVIES_URL } from '../../../(home)/page';
+import MovieInfo from '../../../../components/movie-info';
+import MovieVideos from '../../../../components/movie-videos';
+
+export const metadata = {
+  title: 'Movie',
+};
+
+// async function getMovie(id: string) {
+//   await new Promise((resolve) => setTimeout(resolve, 1000));
+//   const response = await fetch(`${MOVIES_URL}/${id}`);
+//   return response.json();
+// }
+
+// async function getVideos(id: string) {
+//   await new Promise((resolve) => setTimeout(resolve, 5000));
+//   const response = await fetch(`${MOVIES_URL}/${id}/videos`);
+//   return response.json();
+// }
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1>Movie - {id}</h1>;
+  // const movieData = getMovie(id);
+  // const videoData = getVideos(id);
+  // The problem of below line is if user wants to see the UI, two fetches has to be done.
+  // const [movie, video] = await Promise.all([movieData, videoData]);
+  return (
+    <div>
+      <h3>Movie Detail Page</h3>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
